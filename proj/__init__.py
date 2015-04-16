3f3ae5acccdfae213cf428bb3ea5a57841f5e803
Sergey <inoks@mail.ru>
2015-04-16 15:14:26 +0300

    version info

---
+++ proj/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.1.0'
+
+
+def parse_version(version):
+    '''
+    '0.1.2-dev' -> (0, 1, 2, 'dev')
+    '0.1.2' -> (0, 1, 2)
+    '''
+    v = version.split('.')
+    v = v[:-1] + v[-1].split('-')
+    ret = []
+    for p in v:
+        if p.isdigit():
+            ret.append(int(p))
+        else:
+            ret.append(p)
+    return tuple(ret)
+
+VERSION = parse_version(__version__)
