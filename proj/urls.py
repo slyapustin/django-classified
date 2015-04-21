235bb9ff3bf02f5c0a8ed2fb86f748d764e2387d
Sergey <inoks@mail.ru>
2015-04-21 10:05:04 +0300

    cache pages

--- proj/urls.py
+++ proj/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.decorators.cache import cache_page
 
 from dcf import views, feeds, sitemap
 
@@ -8,25 +9,24 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.index),
-
-    url(r'^user/my/delete/(?P<pk>\d+)\.html$', views.delete, name='my-delete'),
+    url(r'^$', cache_page(60 * 15)(views.index)),
 
     url(r'^new$', views.ItemCreateView.as_view(), name='item-new'),
     url(r'^edit/(?P<pk>\d+)$', views.ItemUpdateView.as_view(), name='item-edit'),
 
     # listings
-    url(r'^(?P<pk>\d+)-(?P<slug>[-\w]+)$', views.ItemDetailView.as_view(), name='item'),
-    url(r'^group/(?P<pk>\d+)-(?P<slug>[-\w]+)$', views.GroupDetail.as_view(), name='group'),
+    url(r'^(?P<pk>\d+)-(?P<slug>[-\w]+)$', cache_page(60 * 15)(views.ItemDetailView.as_view()), name='item'),
+    url(r'^group/(?P<pk>\d+)-(?P<slug>[-\w]+)$', cache_page(60 * 15)(views.GroupDetail.as_view()), name='group'),
 
     url(r'^search/', views.SearchView.as_view(), name='search'),
     url(r'^robots\.txt$', views.robots),
 
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemap.sitemaps_dict}),
-    url(r'^rss\.xml$', feeds.LatestItemFeed(), name='rss'),
+    url(r'^rss\.xml$', cache_page(60 * 15)(feeds.LatestItemFeed()), name='rss'),
 
     url(r'^user/$', views.MyItemsView.as_view(), name='my'),
     url(r'^user/profile/$', views.view_profile, name='profile'),
+    url(r'^user/my/delete/(?P<pk>\d+)\.html$', views.delete, name='my-delete'),
 
     # authorization
     url(r'user/', include('social.apps.django_app.urls', namespace='social')),
