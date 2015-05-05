354c6437451eb3d4f02e8dc9c93c9ebc6aba37c6
Sergey <inoks@mail.ru>
2015-05-05 14:02:47 +0300

    Yandex Metrika counter

--- dcf/context_processors.py
+++ dcf/context_processors.py
@@ -7,9 +7,14 @@ def common_values(request):
     data = {}
 
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
+    ya_metrika_prop_id = getattr(settings, 'YANDEX_METRIKA_PROPERTY_ID', False)
+
 
     if not settings.DEBUG and ga_prop_id:
         data['GOOGLE_ANALYTICS_PROPERTY_ID'] = ga_prop_id
+
+    if not settings.DEBUG and ya_metrika_prop_id:
+        data['YANDEX_METRIKA_PROPERTY_ID'] = ya_metrika_prop_id
 
     data['settings'] = settings.DCF
 
