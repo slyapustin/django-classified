ec3b555f1a6e8a6ee23365f339b331a1daf8e941
Sergey <inoks@mail.ru>
2015-05-06 13:23:20 +0300

    google webmaster verification id

--- dcf/context_processors.py
+++ dcf/context_processors.py
@@ -8,6 +8,7 @@ def common_values(request):
 
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
     ya_metrika_prop_id = getattr(settings, 'YANDEX_METRIKA_PROPERTY_ID', False)
+    google_verification_id = getattr(settings, 'GOOGLE_SITE_VERIFICATION_ID', False)
 
 
     if not settings.DEBUG and ga_prop_id:
@@ -15,6 +16,9 @@ def common_values(request):
 
     if not settings.DEBUG and ya_metrika_prop_id:
         data['YANDEX_METRIKA_PROPERTY_ID'] = ya_metrika_prop_id
+
+    if not settings.DEBUG and google_verification_id:
+        data['GOOGLE_SITE_VERIFICATION_ID'] = google_verification_id
 
     data['settings'] = settings.DCF
 
