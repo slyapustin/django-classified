3051300adbd132b543a31103142cd203ee8ad58b
Sergey <inoks@mail.ru>
2015-07-01 16:14:03 +0200

    Open Graph tags

--- dcf/context_processors.py
+++ dcf/context_processors.py
@@ -1,15 +1,16 @@
 # -*- coding:utf-8 -*-
 from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def common_values(request):
 
-    data = {}
-
+    current_site = get_current_site(request)
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
     ya_metrika_prop_id = getattr(settings, 'YANDEX_METRIKA_PROPERTY_ID', False)
     google_verification_id = getattr(settings, 'GOOGLE_SITE_VERIFICATION_ID', False)
 
+    data = {}
 
     if not settings.DEBUG and ga_prop_id:
         data['GOOGLE_ANALYTICS_PROPERTY_ID'] = ga_prop_id
@@ -21,5 +22,6 @@ def common_values(request):
         data['GOOGLE_SITE_VERIFICATION_ID'] = google_verification_id
 
     data['settings'] = settings.DCF
+    data['site'] = 'http://%s' % current_site.domain
 
     return data
