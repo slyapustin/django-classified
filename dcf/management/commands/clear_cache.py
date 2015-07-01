6fc6d35e4e0209455b25866ba241fa36899dca58
Sergey <inoks@mail.ru>
2015-07-01 15:49:42 +0200

    add manage command 'clear_cache'

---
+++ dcf/management/commands/clear_cache.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    
+    help = 'Clear entire cache'
+
+    def handle(self, *args, **options):
+        from django.core.cache import cache
+        cache.clear()
