f7122444dac88f4889a93ea7e701fa68d6c37d17
Sergey <inoks@mail.ru>
2016-04-15 16:07:46 +0200

    Always display GA code if available

--- dcf/context_processors.py
+++ dcf/context_processors.py
@@ -1,21 +1,13 @@
 # -*- coding:utf-8 -*-
 from django.conf import settings
-from django.contrib.sites.shortcuts import get_current_site
 
 
 def common_values(request):
 
-    ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
-    google_verification_id = getattr(settings, 'GOOGLE_SITE_VERIFICATION_ID', False)
-
-    values = {}
-
-    if not settings.DEBUG and ga_prop_id:
-        values['GOOGLE_ANALYTICS_PROPERTY_ID'] = ga_prop_id
-
-    if not settings.DEBUG and google_verification_id:
-        values['GOOGLE_SITE_VERIFICATION_ID'] = google_verification_id
-
-    values['settings'] = settings.DCF
+    values = {
+        'GOOGLE_ANALYTICS_PROPERTY_ID': getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', ''),
+        'GOOGLE_SITE_VERIFICATION_ID': getattr(settings, 'GOOGLE_SITE_VERIFICATION_ID', ''),
+        'settings': settings.DCF
+    }
 
     return values
