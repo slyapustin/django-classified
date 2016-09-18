be6d4b5382ee53fbd71067a8e7cfb274a97b656f
axcel <axcel.github@gmail.com>
2016-09-18 13:18:43 +0500

    favorit add datetime field

--- dcf/static/dcf/js/main.js
+++ dcf/static/dcf/js/main.js
@@ -5,27 +5,26 @@ jQuery(function($){
 
   $("span.star").on("click", function(){
       if ($(this).hasClass("glyphicon-star-empty")) {
-          $(this).toggleClass("glyphicon-star-empty glyphicon-star");
-
+          var that = this
           $.ajax({
             type: "POST",
             url: "/add_favorites/",
             dataType: "json",
             data: { "item": $(this).attr("id") },
-            success: function(data) {
-                data.msg;
+            success: function() {
+                // dont know why 'this' not work, use var that
+                $(that).toggleClass("glyphicon-star-empty glyphicon-star");
             }
           });
       } else {
-          $(this).toggleClass("glyphicon-star glyphicon-star-empty");
-
+          var that = this
           $.ajax({
             type: "POST",
             url: "/del_favorites/",
             dataType: "json",
             data: { "item": $(this).attr("id") },
-            success: function(data) {
-                data.msg
+            success: function() {
+                $(that).toggleClass("glyphicon-star glyphicon-star-empty");
             }
           });
       }
