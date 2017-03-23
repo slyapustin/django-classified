e8c228c81ef28ee6b5849084327279dcccebfc0c
Inoks <inoks@mail.ru>
2017-03-23 21:31:35 +0200

    Implemented basic Structured Data scema https://schema.org/Product Added currency

--- dcf/context_processors.py
+++ dcf/context_processors.py
@@ -9,6 +9,7 @@ def common_values(request):
         'DCF_LOGIN_TO_CONTACT': settings.DCF_LOGIN_TO_CONTACT,
         'DCF_SITE_DESCRIPTION': settings.DCF_SITE_DESCRIPTION,
         'DCF_SITE_NAME': settings.DCF_SITE_NAME,
+        'DCF_CURRENCY': settings.DCF_CURRENCY,
         'GOOGLE_ANALYTICS_PROPERTY_ID': settings.GOOGLE_ANALYTICS_PROPERTY_ID,
         'GOOGLE_SITE_VERIFICATION_ID': settings.GOOGLE_SITE_VERIFICATION_ID,
     }
