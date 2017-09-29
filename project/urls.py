5e41bd31acfa4f0ac9ffd6e65453c81a62f9ad8b
Inoks <inoks@mail.ru>
2017-09-29 22:54:35 +0200

    Removed unused import

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import logout, login
 
 admin.autodiscover()
 
