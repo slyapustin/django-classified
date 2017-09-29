c26cffa888428522aecc982e682e7a67ff4582a0
Inoks <inoks@mail.ru>
2017-09-29 19:38:38 +0200

    Updated egg name

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(
-    name='dcf',
+    name='dcf_core',
     version='0.1',
     description='Django Classifieds Application',
     url='https://github.com/inoks/dcf',
-    author='Inoks',
+    author='Inoks <inoks@mail.ru>',
     author_email='inoks@mail.ru',
 )
