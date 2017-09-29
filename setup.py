697d8c8d69317af927c190ac4638019ab4554179
Inoks <inoks@mail.ru>
2017-09-29 19:32:39 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='dcf',
+    version='0.1',
+    description='Django Classifieds Application',
+    url='https://github.com/inoks/dcf',
+    author='Inoks',
+    author_email='inoks@mail.ru',
+)
