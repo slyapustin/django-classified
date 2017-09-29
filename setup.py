4a9ee05002c2bea0529b8aaa649340dc7f3e7a8d
Inoks <inoks@mail.ru>
2017-09-29 19:51:39 +0200

    Include dcf requirements to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
-
+import os
 from distutils.core import setup
+
+
+def get_requirements():
+    with open(os.path.join(os.path.dirname(__file__), "requirements/base.txt")) as f:
+        requirements_list = [req.strip() for req in f.readlines()]
+
+    # requirements_list.append("setuptools")
+    # requirements_list.append("pytz")
+    return requirements_list
+
 
 setup(
     name='dcf_core',
@@ -9,4 +19,5 @@ setup(
     url='https://github.com/inoks/dcf',
     author='Inoks <inoks@mail.ru>',
     author_email='inoks@mail.ru',
+    install_requires=get_requirements(),
 )
