426ab424753bed1588274c20d58d206a111f399d
Inoks <inoks@mail.ru>
2017-09-29 21:23:05 +0200

    Updated setup app name.

--- setup.py
+++ setup.py
@@ -7,13 +7,11 @@ def get_requirements():
     with open(os.path.join(os.path.dirname(__file__), "requirements/base.txt")) as f:
         requirements_list = [req.strip() for req in f.readlines()]
 
-    # requirements_list.append("setuptools")
-    # requirements_list.append("pytz")
     return requirements_list
 
 
 setup(
-    name='dcf_core',
+    name='dcf',
     version='0.1',
     description='Django Classifieds Application',
     url='https://github.com/inoks/dcf',
