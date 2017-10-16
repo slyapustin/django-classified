6b55f65bac4f4509bc546fe5f3617d8fc8e68b5f
Andrey Motov <tovrey@gmail.com>
2017-10-16 09:12:41 +0300

    Complaint translations ru-RU, import settings

--- dcf/views.py
+++ dcf/views.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
