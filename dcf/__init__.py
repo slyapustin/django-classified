c6736ea51024636770215798ed5c2c11a11b34c9
Inoks <inoks@mail.ru>
2018-07-09 14:36:29 +0200

    Updated setup.py to use setuptools. Bump version.

--- dcf/__init__.py
+++ dcf/__init__.py
@@ -1,4 +1,5 @@
-__version__ = VERSION = '0.5'
+__version__ = VERSION = '0.5.2'
 __version_info__ = tuple(__version__.split('.'))
 
 default_app_config = 'dcf.apps.DCFConfig'
+name = "dcf"
--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
-import os
-from distutils.core import setup
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-def get_requirements():
-    with open(os.path.join(os.path.dirname(__file__), "dcf/requirements.txt")) as f:
-        requirements_list = [req.strip() for req in f.readlines()]
-
-    return requirements_list
-
-
-setup(
-    author='Inoks <inoks@mail.ru>',
-    author_email='inoks@mail.ru',
-    description='Django Classifieds Application',
-    install_requires=get_requirements(),
-    keywords='django classified development bootstrap',
-    license='MIT',
-    name='dcf',
-    url='https://github.com/inoks/dcf',
-    version='0.5',
-    classifiers=[
-        'Development Status :: 3 - Beta',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ]
+setuptools.setup(
+    author='Sergey Lyapustin',
+    name="dcf",
+    version="0.5.2",
+    author_email="inoks@mail.ru",
+    description="Django Classifieds Ads Application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/inoks/dcf",
+    keywords='django classified ads',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
