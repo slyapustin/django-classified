758020cd89205ab5a205d7ab96016bd9dd2b65b3
Inoks <inoks@mail.ru>
2018-07-14 00:30:26 +0200

    Updated jQuery and Lightbox, renamed img -> images.

--- demo/demo_classified/project/urls.py
+++ demo/demo_classified/project/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = [
     url('', include('django_classified.urls', namespace='django_classified')),
@@ -12,3 +14,6 @@ urlpatterns = [
     url('login/', auth_views.LoginView.as_view(template_name='demo/login.html'), name='login'),
     url('email-sent/', TemplateView.as_view(template_name='demo/email_sent.html'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
