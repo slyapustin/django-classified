e0f7d9021a2e6f609ec27ca411122a21e0616957
Sergey Lyapustin <s.lyapustin@gmail.com>
2025-01-05 14:12:47 +0100

    Update GitHub Actions workflow for publishing and adjust versioning scheme

--- django_classified/__init__.py
+++ django_classified/__init__.py
@@ -1,10 +1,10 @@
-VERSION = (1, 1, 0)
+VERSION = (1, 1, "b")
 
 
 def get_version():
     """Return the version as a human-format string."""
     version = f"{VERSION[0]}.{VERSION[1]}"
-    # Append 3rd digit if > 0
+    # Append 3rd symbol if it's not zero
     if VERSION[2]:
         version = f"{version}.{VERSION[2]}"
 
